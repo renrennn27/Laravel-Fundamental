45f07e0fdd3b1ef29980386139740612d09927fc
renrennn27 <renoalifprasetyo20@gmail.com>
2025-10-24 13:45:37 +0700

    migration add column

--- database/migrations/2025_10_24_061542_add_gender_to_siswas.php
+++ database/migrations/2025_10_24_061542_add_gender_to_siswas.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('siswas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->string('gender');
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('siswas');
+        Schema::table('siswas', function (Blueprint $table) {
+            $table->dropColumn('gender');
+        });
     }
 };
