09fd3537dd26ac5416ea39e0f0e834fdfb8b6e25
renrennn27 <renoalifprasetyo20@gmail.com>
2025-10-24 13:11:14 +0700

    Migration Add table

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\SiswaController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/siswa', [SiswaController::class, 'index']);
